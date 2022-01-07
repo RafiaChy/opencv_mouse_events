9b6be85dee1e8b9acbc3f859801d467d17d25244
RafiaChy <rafiachowdhury24@gmail.com>
2022-01-07 22:42:15 +0600

    second commit

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ img = cv2.imread('aero1.jpg', 1)
 def click_mouse(event, x_coordinate, y_coordinate, flags, params):
     font = cv2.FONT_HERSHEY_DUPLEX
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.putText(img, str(x_coordinate) + ',' + str(y_coordinate), (x_coordinate, y_coordinate), font, 2, (0, 0, 255),
-                     3)
+        cv2.putText(img, str(x_coordinate) + ',' + str(y_coordinate), (x_coordinate, y_coordinate), font, 1,
+                    (0, 0, 255),
+                    3)
         cv2.imshow('Image', img)
 
 
-cv2.imshow('Image', img)
+cv2.imshow('Image', img) # update the window instead of creating a different window
 cv2.setMouseCallback('Image', click_mouse)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
